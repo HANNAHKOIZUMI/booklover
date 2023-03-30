d8c9d148bf79943e81bfd922055de2809bfbd36e
HANNAHKOIZUMI <you@example.com>
2023-03-29 21:37:52 -0400

    setup file

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+src
+    booklover
+        __init__.py
+        booklover.py
+booklover_test.py
+
+setup(name='booklover',
+      version='0.1',
+      description='Booklover Package',
+      url='https://github.com/HANNAHKOIZUMI/booklover',
+      author='Hannah Koizumi',
+      author_email='hek3bm@virginia.edu',
+      license='MIT',
+      packages=['booklover'])
